0ba63624200230c5b573e5354c9c796730deaf44
Sebastian <sebastianchoquemamani@gmail.com>
2022-09-05 21:13:19 -0300

    subiendo proyecto

---
+++ Front end/src/components/GenresInDb.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Genre from './Genre';
+
+/*let genres = [
+    {genre: 'Acción'},
+    {genre: 'Animación'},
+    {genre: 'Aventura'},
+    {genre: 'Ciencia Ficción'},
+    {genre: 'Comedia'},
+    {genre: 'Documental'},
+    {genre: 'Drama'},
+    {genre: 'Fantasia'},
+    {genre: 'Infantiles'},
+    {genre: 'Musical'}
+]*/
+
+class GenresInDb extends Component {
+    constructor() {
+        super();
+        this.state = {
+            genresList: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/genres')
+            .then(res => {
+                return res.json()
+            })
+            .then(genres => {
+                this.setState({
+                    genresList: genres.data
+                })
+            })
+            .catch(error => console.log(error))
+    }
+
+    changeWallpaperh6(){
+        document.getElementById("cardBody").classList.add("bg-secondary");
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <h6 onMouseOver={this.changeWallpaperh6} className="m-0 font-weight-bold text-gray-800" >Genres in DataBase</h6>
+                <div className="card-body fondoCaja" id="cardBody">
+                    <div className="row">
+                        {this.state.genresList.map((genre, index) => {
+                            return <Genre {...genre} key={index} />
+                        })}
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+    /*<!-- Categories in DB 
+    <div className="col-lg-6 mb-4">						
+        <div className="card shadow mb-4">
+            <div className="card-header py-3">
+                <h6 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
+            </div>
+            <div className="card-body">
+                <div className="row">
+                    {
+                        genres.map((genre,index)=>{
+                            return  <Genre  {...genre}  key={index} />
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    </div>-->*/
+}
+export default GenresInDb;
